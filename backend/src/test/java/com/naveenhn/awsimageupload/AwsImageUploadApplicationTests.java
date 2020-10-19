b201d1f53c75e764e27658ff863cd5c32423afe9
Naveen-HN <hnnaveen93@gmail.com>
2020-10-19 16:16:32 -0400

    initial commit

---
+++ backend/src/test/java/com/naveenhn/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveenhn.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
