6dbd1f878cdb58c07ffcd42525fad623ada14fe3
Naveen-HN <hnnaveen93@gmail.com>
2020-10-20 20:37:26 -0400

    Added environment file

--- backend/src/main/java/com/naveenhn/awsimageupload/config/AmazonConfig.java
+++ backend/src/main/java/com/naveenhn/awsimageupload/config/AmazonConfig.java
@@ -1,18 +1,16 @@
 package com.naveenhn.awsimageupload.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Configuration
 public class AmazonConfig {
 
     @Bean
-    public AmazonS3 s3(){
-       AWSCredentials awsCredentials = new BasicAWSCredentials(
-        accessKey: 
-           secretKey: 
-       );
-       return AmazonS3ClientBuilder
-       .standard()
-       .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-       .build();
+    public AmazonS3 s3() {
+        Dotenv dotenv = Dotenv.load();
+        AWSCredentials awsCredentials = new BasicAWSCredentials(dotenv.get("ACCESSKEY"), dotenv.get("SECRETKEY"));
+        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
     }
 
 }
